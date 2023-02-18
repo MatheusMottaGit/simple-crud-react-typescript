66de46dc779da2a6e625e7b68bb9f8d2b9b65879
Matheus Motta <matheusdomingues423@gmail.com>
2023-02-18 09:39:18 -0300

    user types

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface UserProps{
+    id?: number
+    name?: string
+    username?: string
+    email?: string
+    adress?: string
+    phone?: string
+    website?: string
+    company?: string
+}
